4feb40656361ced46d40461794179977962ebc90
Chris Foster <cdbfoster@gmail.com>
2020-05-07 18:43:46 -0400

    Add the network manager to context.

--- src/context.rs
+++ src/context.rs
@@ -1,8 +1,10 @@
 use crate::layer::manager::LayerManager;
+use crate::network::NetworkInterface;
 
 #[derive(Debug)]
 pub struct Context {
     pub layer_manager: LayerManager,
+    pub network_interface: NetworkInterface,
 }
 
 impl Context {
