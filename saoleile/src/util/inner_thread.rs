d34c4f154ce8f869c82c1b92c226a1739cc5ac43
Chris Foster <cdbfoster@gmail.com>
2020-05-07 21:48:45 -0400

    Added InnerThread.

---
+++ saoleile/src/util/inner_thread.rs
@@ -0,0 +1,14 @@
+use std::sync::Mutex;
+use std::thread;
+
+pub struct InnerThread(Mutex<Option<thread::JoinHandle<()>>>);
+
+impl InnerThread {
+    pub fn new(join_handle: thread::JoinHandle<()>) -> Self {
+        Self(Mutex::new(Some(join_handle)))
+    }
+
+    pub fn join(&self) {
+        self.0.lock().unwrap().take().unwrap().join().ok();
+    }
+}
