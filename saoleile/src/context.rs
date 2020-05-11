36febb7a518641fc442b008a69273fa35226ca3d
Chris Foster <cdbfoster@gmail.com>
2020-05-10 23:07:38 -0400

    Added some body to Context.

--- saoleile/src/context.rs
+++ saoleile/src/context.rs
@@ -1,3 +1,6 @@
+use std::net::SocketAddr;
+use std::sync::Mutex;
+
 use crate::layer::manager::LayerManager;
 use crate::network::NetworkInterface;
 use crate::scene::SceneManager;
@@ -9,10 +12,40 @@ pub struct Context {
     pub scene_manager: SceneManager,
     pub timer_manager: TimerManager,
     pub network_interface: NetworkInterface,
+
+    running: Mutex<bool>,
 }
 
 impl Context {
+    pub fn new(network_bind_address: SocketAddr) -> Self {
+        Self {
+            layer_manager: LayerManager::new(),
+            scene_manager: SceneManager::new(),
+            timer_manager: TimerManager::new(),
+            network_interface: NetworkInterface::new(network_bind_address),
+            running: Mutex::new(true),
+        }
+    }
+
+    pub fn is_running(&self) -> bool {
+        *self.running.lock().unwrap()
+    }
+
     pub fn shutdown(&self) {
-        self.layer_manager.shutdown();
+        let mut running = self.running.lock().unwrap();
+
+        if *running {
+            self.layer_manager.shutdown();
+            self.scene_manager.shutdown();
+            self.timer_manager.shutdown();
+            self.network_interface.shutdown();
+            *running = false;
+        }
+    }
+}
+
+impl Drop for Context {
+    fn drop(&mut self) {
+        self.shutdown();
     }
 }
